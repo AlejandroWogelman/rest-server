0538f0ad5d2ee324c0052a15155aabfc7464b3ab
Alejandro Wogelman <alehellamn@gmail.com>
2023-04-22 00:21:33 -0300

    Google identity | login-logout-new

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const { response } = require("express");
 const Usuario = require("../models/usuario");
 const bcryptjs = require("bcryptjs");
 const { generarJWT } = require("../helpers/generarJWT");
-bcryptjs;
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
@@ -43,4 +43,54 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { nombre, img, correo } = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      // Si no existe, hay que crearlo.
+
+      const data = {
+        nombre,
+        correo,
+        password: ":P",
+        img,
+        google: true,
+        rol: "USER_ROLE",
+      };
+      usuario = new Usuario(data);
+      console.log(usuario);
+
+      usuario = new Usuario(data);
+
+      await usuario.save();
+    }
+
+    // Si el usuario fue marcado como eliminado (estado: false)
+    if (!usuario.estado) {
+      res.status(401).json({
+        msg: "Hable con el administrador, usuario bloqueado",
+      });
+    }
+
+    //Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(400).json({
+      ok: false,
+      msg: "El token no se pudo verificar pa",
+    });
+  }
+};
+module.exports = { login, googleSignIn };
